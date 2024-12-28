f7fcd34fac17165441df424d59e73866b73b91cd
Unconventional <141981043+Un-conventional@users.noreply.github.com>
2024-12-28 00:35:38 -0500

    Small fixes (V 0.0.9.75)
    Fixed a FAQ format issue
    
    Fixed "New note volume" for pulse width defaulting to 100% instead of 50%.
    
    Fixed Wrong Version number.

--- editor/EditorConfig.ts
+++ editor/EditorConfig.ts
@@ -23,7 +23,7 @@ export function prettyNumber(value: number): string {
 }
 
 export class EditorConfig {
-    public static readonly version: string = "0.0.9.65"; // Currently using patch versions in display (unlike JB)
+    public static readonly version: string = "0.0.9.75"; // Currently using patch versions in display (unlike JB)
     public static readonly versionDisplayName: string = "Unbox " + EditorConfig.version;
 
     public static readonly releaseNotesURL: string = "./patch_notes.html";
